12926b605cf7a7ee73b984b0a0ed4c49eb2927b7
jrueg <jesus.rueg@gmail.com>
2015-07-09 11:38:59 +0200

    Cambio del script de inicio

--- inicia.sh
+++ inicia.sh
@@ -1,4 +1,9 @@
-
+echo Iniciando Servo Blaster
+cd /home/pi/ServoBlaster/
+./servod --p1pins="11,12"
+cd /home/pi/robot_seguidor/
+echo Descargando ultima version desde Github
+git pull
 echo Compilando programa...
 g++ -std=c++0x -o robot *.cpp -I/usr/local/include/ -lraspicam -lraspicam_cv -lrt -lmmal -lmmal_core -lmmal_util -lopencv_core -lopencv_highgui -lopencv_imgproc -L/opt/vc/lib -lpthread -lwiringPi
 echo Fin compilado.
